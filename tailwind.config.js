ded794bdae74097fd94cbb43a6c7359efc1ad929
Ritik <singhritik2675@gmail.com>
2025-03-13 09:46:00 +0530

    Fixed conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-<<<<<<< HEAD
-    extend: {
-      screens: {
-        'xs': {max: "375px"},
-      }
-    },
-=======
     extend: {},
->>>>>>> c57c5d27929ad29bb19691e28893714773867d53
   },
   plugins: [],
-}
+};
